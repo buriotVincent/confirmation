bc7ea652b1bcfba5efac038092bdebac985b7122
buriotVincent <buriot.vincent@gmail.com>
2019-06-04 14:38:21 +0200

    test1

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var urlencodedParser = bodyParser.urlencoded({extended: false});ù
+
+var app = express();
+
+app.use('/home/confirmation/', function(req, res) {
+     res.render('confirmation.ejs');
+});
+
+app.use('/home/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil');
+})
+
+app.post('/home/confirmation/', urlencodedParser, function(req, res) {
+    if (req.body.confirm !== '') {
+        console.log(req.body.confirm);
+    }
+    res.redirect('/home');
+});
+
+app.use(function(req, res) {
+    res.redirect('/home')
+});
+
+app.listen(3389);
